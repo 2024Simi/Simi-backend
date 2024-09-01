a3eded2f7fc24e1718d0d8b46f6b980fbbd188bb
myeongju00 <j51677767@gmail.com>
2024-09-01 14:37:09 +0900

    first init

---
+++ src/main/java/com/project/simi/SimiApplication.java
@@ -0,0 +1,13 @@
+package com.project.simi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/simi/SimiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.simi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
