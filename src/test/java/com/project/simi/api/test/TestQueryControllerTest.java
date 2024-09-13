f09897d2dac9bef3bbc605c912ee835bec6c4046
yuhyeongchan <gudcks305@gmail.com>
2024-09-13 22:43:33 +0900

    test code and db settings

--- src/test/java/com/project/simi/api/test/TestQueryControllerTest.java
+++ src/test/java/com/project/simi/api/test/TestQueryControllerTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
-public class TestController extends SuperIntegrationTest {
+public class TestQueryControllerTest extends SuperIntegrationTest {
 
     @Autowired
     @Qualifier("testBean")
