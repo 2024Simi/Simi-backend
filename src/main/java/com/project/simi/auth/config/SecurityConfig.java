b114bb897a16590aee10973d0c73707952a4045f
yuhyeongchan <gudcks305@gmail.com>
2024-09-26 21:13:47 +0900

    feat : temp

---
+++ src/main/java/com/project/simi/auth/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.project.simi.auth.config;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import com.project.simi.filter.JwtAuthenticationFilter;
+
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CorsConfigurationSource corsConfigurationSource;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource))
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests(
+                        authz ->
+                                authz.requestMatchers("/api/v1/login")
+                                        .permitAll()
+                                        .anyRequest()
+                                        .authenticated())
+                .addFilterBefore(
+                        jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    @Profile("!prod")
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web ->
+                web.ignoring()
+                        .requestMatchers(
+                                "/resources/**",
+                                "/static/**",
+                                "/actuator/**",
+                                /* swagger */
+                                "/swagger-ui/**",
+                                "/swagger-resources/**",
+                                "/v3/api-docs/**",
+                                /* external callback */
+                                // TODO : 2024-06-13 15:17 추후 토큰 인증으로 변경
+                                "/api/v1/callback/**");
+    }
+}
---
+++ src/main/java/com/project/simi/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,98 @@
+package com.project.simi.filter;
+
+import static org.springframework.util.StringUtils.hasText;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.project.simi.auth.provider.JwtTokenProvider;
+import com.project.simi.common.exception.UnauthorizedException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    //    private final UserQueryRepository userQueryRepository;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        try {
+            extractToken(authorization)
+                    .ifPresentOrElse(
+                            jwtToken -> {
+                                jwtTokenProvider.validateToken(jwtToken);
+
+                                Authentication authentication =
+                                        getAuthentication(jwtTokenProvider.getSubject(jwtToken));
+                                SecurityContextHolder.getContext()
+                                        .setAuthentication(authentication);
+
+                                //                                buildMDC(authentication);
+                            },
+                            () -> {
+                                // TODO : 2024-02-08 16:04 토큰이 없는 경우 동작 처리 해야할까?
+                            });
+
+            filterChain.doFilter(request, response);
+        } catch (UnauthorizedException e) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        }
+    }
+
+    private Optional<String> extractToken(String authorization) { // resolve AccessToken
+        if (hasText(authorization) && Pattern.matches("^Bearer .*", authorization)) {
+            String value = authorization.replaceAll("^Bearer( )*", "");
+
+            return hasText(value) ? Optional.of(value) : Optional.empty();
+        }
+
+        return Optional.empty();
+    }
+
+    private Authentication getAuthentication(String subject) {
+        long userId;
+        try {
+            userId = Long.parseLong(subject);
+        } catch (NumberFormatException e) {
+            throw new UnauthorizedException();
+        }
+        /*
+        User user = userQueryRepository.getById(userId);
+        AuthenticatedUser authenticatedUser = new AuthenticatedUser(user);
+
+        return new UsernamePasswordAuthenticationToken(
+                authenticatedUser, null, authenticatedUser.getAuthorities());*/
+        return new UsernamePasswordAuthenticationToken(null, null, null);
+    }
+
+    /* private void buildMDC(Authentication authentication) {
+        if (authentication != null && authentication.getPrincipal() instanceof RequestUser user) {
+            MDC.put("user_id", user.getUserId().toString());
+            MDC.put("company_id", user.getCompanyId().toString());
+            MDC.put("name", user.getName());
+        }
+    }*/
+}
