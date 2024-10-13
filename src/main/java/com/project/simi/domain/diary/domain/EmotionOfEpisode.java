c1f94a9b6a88d7c976e1dfc287e9885398e850c7
myeongju00 <j51677767@gmail.com>
2024-10-13 18:22:51 +0900

    refactor : EmotionOfEpisode record 탕입으로 변경

--- src/main/java/com/project/simi/domain/diary/domain/EmotionOfEpisode.java
+++ src/main/java/com/project/simi/domain/diary/domain/EmotionOfEpisode.java
@@ -2,24 +2,7 @@ package com.project.simi.domain.diary.domain;
 
 import java.util.List;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-@Getter
-@EqualsAndHashCode
-@NoArgsConstructor
-public class EmotionOfEpisode {
-    @JsonProperty("type")
-    private String type; // 감정의 유형
-
-    @JsonProperty("details")
-    private List<String> details; // 세부적인 감정 리스트
-
-    public EmotionOfEpisode(String type, List<String> details) {
-        this.type = type;
-        this.details = details;
-    }
-}
+public record EmotionOfEpisode(
+        @JsonProperty("type") String type, @JsonProperty("details") List<String> details) {}
