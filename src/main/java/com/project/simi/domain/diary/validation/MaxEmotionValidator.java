da0dad5c4726bda27453f6bfda2bdf43ff73b237
myeongju00 <j51677767@gmail.com>
2024-11-06 17:37:00 +0900

    refactor : 감정 개수 계산 메서드 분리 및 최대 감정 수 상수화

--- src/main/java/com/project/simi/domain/diary/validation/MaxEmotionValidator.java
+++ src/main/java/com/project/simi/domain/diary/validation/MaxEmotionValidator.java
@@ -10,18 +10,21 @@ import com.project.simi.domain.diary.dto.DiaryDto.EmotionOfEpisodeDto;
 public class MaxEmotionValidator
         implements ConstraintValidator<MaxEmotionCheck, List<EmotionOfEpisodeDto>> {
 
+    private static final int MAX_EMOTIONS = 5;
+
     @Override
     public boolean isValid(
             List<EmotionOfEpisodeDto> emotionList, ConstraintValidatorContext context) {
-        int totalEmotions =
-                emotionList.stream().mapToInt(emotion -> emotion.details().size()).sum();
-        if (totalEmotions > 5) {
+        if (getTotalEmotions(emotionList) > MAX_EMOTIONS) {
             context.disableDefaultConstraintViolation();
             context.buildConstraintViolationWithTemplate("선택할 수 있는 감정은 최대 5개 입니다.")
                     .addConstraintViolation();
             return false;
         }
-
         return true;
+    }
+
+    private int getTotalEmotions(List<EmotionOfEpisodeDto> emotionList) {
+        return emotionList.stream().mapToInt(emotion -> emotion.details().size()).sum();
     }
 }
