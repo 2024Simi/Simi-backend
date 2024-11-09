cb0de2c651697ad84e347c82c7ff40c3adff6328
myeongju00 <j51677767@gmail.com>
2024-11-09 21:07:12 +0900

    fix : post -> put으로 변경

--- src/main/java/com/project/simi/domain/diary/controller/DiaryController.java
+++ src/main/java/com/project/simi/domain/diary/controller/DiaryController.java
@@ -12,6 +12,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -50,7 +51,7 @@ public class DiaryController {
         return ApiResult.ok(response);
     }
 
-    @PostMapping("diary/{diaryId}")
+    @PutMapping("diary/{diaryId}")
     public ApiResult<DefaultIdResponse> updateDiary(
             @PathVariable Long diaryId, @Valid @RequestBody DiaryUpdateRequest request) {
         Long response = diaryService.updateDiary(diaryId, request);
