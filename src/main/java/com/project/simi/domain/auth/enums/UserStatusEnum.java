d2a9eb0224824d943eacbfa4d97fa7f3ff227c81
myeongju00 <j51677767@gmail.com>
2024-12-07 17:05:52 +0900

    fix : 회원가입 완료 -> 회원가입으로 수정

--- src/main/java/com/project/simi/domain/auth/enums/UserStatusEnum.java
+++ src/main/java/com/project/simi/domain/auth/enums/UserStatusEnum.java
@@ -7,8 +7,8 @@ import com.project.simi.domain.common.dto.DescriptionEnum;
 
 @Getter
 @AllArgsConstructor
-public enum LoginFlagEnum implements DescriptionEnum {
-    SIGN_IN("회원가입 완료"),
+public enum UserStatusEnum implements DescriptionEnum {
+    SIGN_IN("회원가입"),
     NICKNAME_PENDING("약관 동의까지 완료"),
     LOGIN("로그인"),
     WITHDRAWAL("탈퇴"),
